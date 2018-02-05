0d946c9a2aba816fec3dc153f2f4c75f5f26e6c1
BehnoudSh <behnoud.sherafati@gmail.com>
2018-02-05 14:51:17 +0330

    bus

--- app/src/main/java/ir/chichand/chichand/Activity/BusActivity.java
+++ app/src/main/java/ir/chichand/chichand/Activity/BusActivity.java
@@ -1,9 +1,11 @@
 package ir.chichand.chichand.Activity;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.DatePicker;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -14,7 +16,7 @@ import ir.chichand.chichand.Fragments.Fragment_BusSearch;
 import ir.chichand.chichand.R;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
-public class BusActivity extends AppCompatActivity {
+public class BusActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     @BindView(R.id.tv_actionbar_title)
     TextView tv_actionbar_title;
@@ -36,15 +38,20 @@ public class BusActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
 
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        Fragment_BusSearch busSearchFragment = Fragment_BusSearch.newInstance();
-        fragmentTransaction.add(R.id.frame, busSearchFragment, "busSearchFragment");
-        fragmentTransaction.addToBackStack("busSearchFragment");
-        fragmentTransaction.commit();
+//        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+//        Fragment_BusSearch busSearchFragment = Fragment_BusSearch.newInstance();
+//        fragmentTransaction.add(R.id.frame, busSearchFragment, "busSearchFragment");
+//        fragmentTransaction.addToBackStack("busSearchFragment");
+//        fragmentTransaction.commit();
 
         tv_actionbar_title.setText("بلیت اتوبوس");
 
         actionbarholder.setBackgroundColor(getResources().getColor(R.color.holder3));
+
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
 
     }
 }
