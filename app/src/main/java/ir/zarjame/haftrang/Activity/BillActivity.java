064dbd3b4ddac610da4bced3c30dfb321f9ae713
behnoud <behnoud.sherafati@gmail.com>
2018-05-28 21:30:26 +0430

    قبض

--- app/src/main/java/ir/zarjame/haftrang/Activity/BillActivity.java
+++ app/src/main/java/ir/zarjame/haftrang/Activity/BillActivity.java
@@ -1,11 +1,13 @@
 package ir.zarjame.haftrang.Activity;
 
-import android.app.ProgressDialog;
+import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,21 +17,18 @@ import android.widget.Toast;
 
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
+import com.google.zxing.common.StringUtils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import ir.zarjame.haftrang.Dialog.Dialog_Barcode_Camera;
 import ir.zarjame.haftrang.Fragments.BillConfirmFragment;
-import ir.zarjame.haftrang.Fragments.ChargeConfirmFragment;
-import ir.zarjame.haftrang.Models.Requests.Request_Bill;
-import ir.zarjame.haftrang.Models.Requests.Request_Charge;
-import ir.zarjame.haftrang.Models.Responses.Response_ChargeReseller;
-import ir.zarjame.haftrang.NetworkServices.ApiCallbacks;
-import ir.zarjame.haftrang.NetworkServices.ApiHandler;
 import ir.zarjame.haftrang.R;
-import ir.zarjame.haftrang.Tools.PublicTools;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
-public class BillActivity extends AppCompatActivity {
+
+public class BillActivity extends AppCompatActivity implements Dialog_Barcode_Camera.Callback {
     @BindView(R.id.toolbar)
     android.support.v7.widget.Toolbar
             toolbar;
@@ -52,6 +51,20 @@ public class BillActivity extends AppCompatActivity {
     @BindView(R.id.btn_confirm)
     Button btn_confirm;
 
+
+    String BillId;
+    String PayId;
+    String BillIdPad13;
+    String PayIdPad13;
+    String BarCode;
+    String ControllPar1;
+    String ControllPar2;
+    String ControllBill;
+    String CheckDigitBillIdPad13;
+    String CheckDigitPayIdPad13;
+    String Ragham2;
+
+
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
@@ -62,6 +75,7 @@ public class BillActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bill);
         ButterKnife.bind(this);
+
 
         int color = getIntent().getIntExtra("bg_color", getResources().getColor(R.color.colorPrimary));
 
@@ -123,8 +137,159 @@ public class BillActivity extends AppCompatActivity {
             return false;
         }
 
+        if (PayId.length() < 5) {
+            Toast.makeText(this, "شناسه پرداخت را به درستی وارد نمایید", Toast.LENGTH_SHORT).show();
+            YoYo.with(Techniques.Shake)
+                    .duration(700)
+                    .playOn(et_payId);
+
+            return false;
+        }
+
+
+        BillIdPad13 = ir.zarjame.haftrang.Tools.StringUtils.leftPad(et_billId.getText().toString(), 13, '0');
+        PayIdPad13 = ir.zarjame.haftrang.Tools.StringUtils.leftPad(et_payId.getText().toString(), 13, '0');
+
+
+        BarCode = BillIdPad13 + PayIdPad13;
+        ControllPar1 = BarCode.substring(24, 25);
+        ControllPar2 = BarCode.substring(25, 26);
+        ControllBill = BarCode.substring(12, 13);
+        CheckDigitBillIdPad13 = BillIdPad13.substring(0, 12);
+        CheckDigitPayIdPad13 = PayIdPad13.substring(0, 11);
+        Ragham2 = BillIdPad13 + PayIdPad13.substring(0, PayIdPad13.length() - 1);
+
+
+        if (!IsValidBill()) {
+            Toast.makeText(this, "قبض نامعتبر", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+
         return true;
 
     }
+
+
+    @Override
+    public void BarcodeCamera_onSuccess(int position, String text) {
+        // beepManager.playBeepSoundAndVibrate();
+
+        if (text.length() >= 13) {
+
+            try {
+
+                BillId = text.substring(0, 13);
+                PayId = text.substring(13);
+                et_billId.setText(BillId);
+                et_payId.setText(PayId);
+                if (validation()) {
+                    btn_confirm.performClick();
+                }
+
+
+            } catch (Exception ex) {
+                // showSnack(getResources().getString(R.string.invalid_info));
+            }
+        } else {
+            // showSnack(getResources().getString(R.string.invalid_info));
+
+        }
+    }
+
+    @Override
+    public void BarcodeCamera_onCanceled() {
+
+    }
+
+
+    @OnClick(R.id.btn_barcode)
+    void btbBarcodeClick(View v) {
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    1);
+        } else {
+            Dialog_Barcode_Camera dialog_barcode_camera = Dialog_Barcode_Camera.newInstance(0, true);
+            dialog_barcode_camera.show(getSupportFragmentManager(), "");
+        }
+
+    }
+
+    private boolean IsValidBill() {
+        return IsValidBillId() && IsValidPayId();
+    }
+
+    private boolean IsValidBillId() {
+
+        boolean isValidBillId = ControllBill.matches(getBillCheckDigit1(CheckDigitBillIdPad13));
+
+        return isValidBillId;
+    }
+
+    private boolean IsValidPayId() {
+
+        boolean isValidPayId = ControllPar1.matches(getBillCheckDigit1(CheckDigitPayIdPad13));
+
+        return isValidPayId;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Dialog_Barcode_Camera dialog_barcode_camera = Dialog_Barcode_Camera.newInstance(0, true);
+                            dialog_barcode_camera.show(getSupportFragmentManager(), "");
+                        }
+                    }, 500);
+
+
+                } else {
+
+
+                    Toast.makeText(this, "برای استفاده از اسکنر بارکد به تایید دسترسی دوربین نیاز است", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+
+
+    public String getBillCheckDigit1(String input) {
+        input = ir.zarjame.haftrang.Tools.StringUtils.leftPad(input, 12, '0');
+        int sum = 0;
+        sum = sum + Integer.parseInt(String.valueOf(input.charAt(11))) * 2;
+        sum = sum + Integer.parseInt(String.valueOf(input.charAt(10))) * 3;
+        sum = sum + Integer.parseInt(String.valueOf(input.charAt(9))) * 4;
+        sum = sum + Integer.parseInt(String.valueOf(input.charAt(8))) * 5;
+        sum = sum + Integer.parseInt(String.valueOf(input.charAt(7))) * 6;
+        sum = sum + Integer.parseInt(String.valueOf(input.charAt(6))) * 7;
+        sum = sum + Integer.parseInt(String.valueOf(input.charAt(5))) * 2;
+        sum = sum + Integer.parseInt(String.valueOf(input.charAt(4))) * 3;
+        sum = sum + Integer.parseInt(String.valueOf(input.charAt(3))) * 4;
+        sum = sum + Integer.parseInt(String.valueOf(input.charAt(2))) * 5;
+        sum = sum + Integer.parseInt(String.valueOf(input.charAt(1))) * 6;
+        sum = sum + Integer.parseInt(String.valueOf(input.charAt(0))) * 7;
+
+        int mode = sum % 11;
+        if ((mode == 0 | mode == 1)) {
+            mode = 0;
+        } else {
+            mode = 11 - mode;
+        }
+        return String.valueOf(mode);
+    }
+
 
 }
---
+++ app/src/main/java/ir/zarjame/haftrang/Dialog/Dialog_Barcode_Camera.java
@@ -0,0 +1,126 @@
+package ir.zarjame.haftrang.Dialog;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+
+import com.google.zxing.ResultPoint;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import ir.zarjame.haftrang.R;
+
+/**
+ * Created by tinabehnoud on 5/24/18.
+ */
+
+public class Dialog_Barcode_Camera extends android.support.v4.app.DialogFragment implements BarcodeCallback {
+
+
+    AppCompatActivity activity;
+    private Unbinder unbinder;
+    Callback callback;
+
+    @BindView(R.id.barcode_scanner)
+    DecoratedBarcodeView barcodeView;
+
+    public Dialog_Barcode_Camera() {
+    }
+
+    public static Dialog_Barcode_Camera newInstance(int position, boolean isParentActivity) {
+        Dialog_Barcode_Camera frag = new Dialog_Barcode_Camera();
+        Bundle args = new Bundle();
+        args.putInt("position", position);
+        args.putBoolean("isParentActivity", isParentActivity);
+        frag.setArguments(args);
+        return frag;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_barcode_camera, container);
+        unbinder = ButterKnife.bind(this, view);
+        activity = (AppCompatActivity) getActivity();
+        barcodeView.decodeContinuous(this);
+        barcodeView.setStatusText("بارکد قبض را به درستی روبروی دوربین قرار دهید");
+
+        if (getArguments().getBoolean("isParentActivity"))
+            callback = (Callback) getActivity();
+        else
+            callback = (Callback) getParentFragment();
+        return view;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+
+        // paymentRequest a window without the title
+        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+
+        // make dialog's background transparent
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        return dialog;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        barcodeView.resume();
+        getDialog().getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        barcodeView.pause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        callback.BarcodeCamera_onCanceled();
+        unbinder.unbind();
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        super.onDismiss(dialog);
+        callback.BarcodeCamera_onCanceled();
+    }
+
+    @Override
+    public void barcodeResult(BarcodeResult result) {
+        if (result.getText() == null) {
+            return;
+        }
+        callback.BarcodeCamera_onSuccess(getArguments().getInt("position", 0), result.getText());
+        dismiss();
+    }
+
+    @Override
+    public void possibleResultPoints(List<ResultPoint> resultPoints) {
+
+    }
+
+    public interface Callback {
+        void BarcodeCamera_onSuccess(int position, String text);
+
+        void BarcodeCamera_onCanceled();
+    }
+
+
+}
